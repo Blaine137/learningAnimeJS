bff85e80e6b74e7723f0c3ba49d4f3a334c00c36
blaine young <blaine.young3553@gmail.com>
2021-04-01 09:27:17 -0400

    tinkered with anime props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Anime from 'react-anime';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Anime opacity={[0,1]}>
+          <img src={logo} className="App-logo" alt="logo" />
+        </Anime>
+        <Anime translateX={[100, 0]}>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </Anime>
+        <Anime translateX={[-100, 0]}>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </Anime>
       </header>
     </div>
   );
